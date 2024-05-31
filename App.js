9feb09768fe36693c2f8cc0d7c9a2ac8d9960826
Rflyy <105841105421@student.unismuh.ac.id>
2024-05-31 11:06:02 +0800

    TugasArsky

--- App.js
+++ App.js
@@ -1,27 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { setStatusBarBackgroundColor } from 'expo-status-bar'
 
-export default function App() {
+const App =() => {
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: "row",
-      }}>
-      <View
-        style={{
+    <View style={{
+      flex: 1,
+    }}>
+
+<View style={{
+  flex: 1,
+  flexDirection:'row'
+}}>
+          <View style={{
+          flex: 1, 
+          backgroundColor: 'white',
+          }}>
+        </View>
+        <View style={{
           flex: 1,
-          backgroundColor: "red",
-        }}
-      ></View>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "blue",
-        }}
-      ></View>
+          backgroundColor: 'purple',
+          }}>
     </View>
-  );
+    </View>
+      <View style={{
+        flex: 1,
+        backgroundColor: 'pink',
+        }}> 
+        </View>
+
+      <View style={{
+        flex: 1,
+        backgroundColor: 'blue',
+        }}></View>
+      
+
+    </View>
+
+    
+  )
 }
 
-const styles = StyleSheet.create({});
+export default App
