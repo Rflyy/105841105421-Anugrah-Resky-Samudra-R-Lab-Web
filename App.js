a0d86945fcce6434a4c1c78d43e002fc0f516341
Rflyy <105841105421@student.unismuh.ac.id>
2024-06-27 17:37:32 +0800

    TextInputCostum

--- App.js
+++ App.js
@@ -1,36 +1,35 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
-import { View, Text } from 'react-native'
 
-const ButtonCustom = ({ text, color }) => (
-  <View style={{
-    backgroundColor: color,
-    width: 250,
-    height: 100,
-    borderRadius: 10,
-    justifyContent: 'center',
-    marginBottom: 10,
-  }}>
-    <Text style={{
-      textAlign: 'center',
-      color: 'white',
-      fontSize: 30,
-      fontWeight: 'bold',
+const TextInputCostum =({placeholder, color, typeKeybooard}) => {
+  return(
+    <TextInput  
+    placeholder={placeholder}
+    keyboardType={typeKeybooard}
+    style ={{
+      width :250,
+      height : 50,
+      borderColor : color,
+      borderWidth : 1,
+      borderRadius : 10,
+      marginBottom :10,
+      paddingLeft : 10
+    }}></TextInput>
+  )
+}
+
+const App = () => {
+  return (
+    <View style ={{
+      flex : 1,
+      justifyContent : 'center',
+      alignItems : 'center',
     }}>
-      {text}
-    </Text>
-  </View>
-)
-
-const App = () => (
-  <View style={{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }}>
-    <ButtonCustom text="Login" color="red" />
-    <ButtonCustom text="Logout" color="blue" />
-    <ButtonCustom text="Register" color="green" />
-  </View>
-)
+      <TextInputCostum placeholder = "Username" color = "black" />
+      <TextInputCostum placeholder = "Password" color = "orange" />
+      <TextInputCostum placeholder = "Gmail" color = "blue" />
+    </View>
+  )
+}
 
 export default App
